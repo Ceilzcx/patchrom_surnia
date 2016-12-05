0fcf4f0990f4380d8f091ef66d1823f55a402d44
Juan Cortes <fam.cortesgutierrez@hotmail.com>
2016-12-04 19:15:20 -0500

    initial port

---
+++ phoneapps.mk
@@ -0,0 +1,31 @@
+local-phone-apps = $(private-phone-apps)
+local-phone-priv-apps = $(private-phone-priv-apps)
+
+private-phone-apps := \
+	Bluetooth \
+	BluetoothExt \
+	CertInstaller \
+	KeyChain \
+	LiveWallpapers \
+	LatinIME \
+	PacProcessor \
+	PrintSpooler \
+	telresources \
+	WAPPushManager
+
+private-phone-priv-apps := \
+		AudioFX \
+		BackupRestoreConfirmation \
+		CarrierConfig \
+		CellBroadcastReceiver \
+		CMSettingsProvider \
+		CMAudioService \
+		ThemeManagerService \
+		ExternalStorageProvider \
+		FusedLocation \
+		InputDevices \
+		ProxyHandler \
+		StatementService \
+		SharedStorageBackup \
+		Shell \
+		ThemesProvider
